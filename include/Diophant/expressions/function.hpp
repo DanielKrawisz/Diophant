b76787fab3fff61a11cafdd6c9339455d0b7640a
Daniel Krawisz <danielkrawisz@protonmail.com>
2024-06-12 09:31:50 -0500

    Add built-in functions

--- include/Diophant/expressions/function.hpp
+++ include/Diophant/expressions/function.hpp
@@ -32,16 +32,75 @@ namespace Diophant::expressions {
         Type operator * () const;
     };
 
+    template <typename T>
+    struct to {
+        const T &operator () (Expression &x) const {
+            return static_cast<const value<T> *> (x.get ())->val;
+        }
+    };
+
+    template <typename T>
+    struct to<const T &> {
+        const T &operator () (Expression &x) const {
+            return static_cast<const value<T> *> (x.get ())->val;
+        }
+    };
+
+    template <typename T>
+    struct from {
+        Expression &operator () (const T &) const;
+    };
+
+    template <typename T> struct function_call;
+
+    template <typename T> T take_first (data::list<T> &l) {
+        auto L = l;
+        l = data::rest (l);
+        return data::first (L);
+    }
+
+    template <typename Return, typename Arg1>
+    struct function_call<Return (Arg1)> {
+        Expression operator () (Return (*f) (Arg1), data::list<Expression> l) {
+            return from<Return> {} (f (to<Arg1> {} (take_first (l))));
+        }
+    };
+
+    template <typename Return, typename Arg1, typename Arg2>
+    struct function_call<Return (Arg1, Arg2)> {
+        Expression operator () (Return (*f) (Arg1, Arg2), data::list<Expression> l) {
+            return from<Return> {} (f (to<Arg1> {} (take_first (l)), to<Arg2> {} (take_first (l))));
+        }
+    };
+
+    template <typename Return, typename Arg1, typename Arg2, typename Arg3>
+    struct function_call<Return (Arg1, Arg2, Arg3)> {
+        Expression operator () (Return (*f) (Arg1, Arg2, Arg3), data::list<Expression> l) {
+            return from<Return> {} (f (to<Arg1> {} (take_first (l)), to<Arg2> {} (take_first (l)), to<Arg3> {} (take_first (l))));
+        }
+    };
+
     template <typename Return, typename... Args>
     struct built_in<Return (Args...)> final : function {
+        Return (*Fun) (Args...);
+
         Type signature () const override {
             return *expressions::signature<Return (Args...)> {};
         }
 
-        Expression operator () (data::list<Expression>) const override;
-        bool operator == (const abstract &) const final override;
+        Expression operator () (data::list<Expression> l) const override {
+            return function_call<Return (Args...)> {} (Fun, l);
+        }
 
-        built_in (Return (*f) (Args...));
+        bool operator == (const abstract &a) const final override {
+            try {
+                return Fun == dynamic_cast<const built_in &> (a).Fun;
+            } catch (std::bad_cast) {
+                return false;
+            }
+        }
+
+        built_in (Return (*f) (Args...)) : Fun {f} {}
     };
     
 }
